e7d9659bc08cac5bded1fc606a68e474b2324e72
JunJangE <69571848+junjange@users.noreply.github.com>
2021-05-17 19:43:48 +0900

    Update 수퍼 바이러스.py

--- softeer/수퍼 바이러스.py
+++ softeer/수퍼 바이러스.py
@@ -1,22 +1,24 @@
 import sys
-# k가 0.1초당 p배씩 증가
-#  n초동안
+
+
+# 지수를 재귀적으로 나눈다.
 def f(x, y):
-    if y ==1:
+    print(x, y)
+    if y == 1:
         return x
 
-    elif y%2 == 0:
-        a = f(x, y/2)
-        return a*a%1000000007
+    elif y % 2 == 0:
+        a = f(x, y / 2)
+        return a * a % 1000000007
 
     else:
-        b = f(x, (y-1)/2)
-        return b*b*x%1000000007
-
-k, p, n = list(map(int,sys.stdin.readline().split()))
-n = 10*n
-result = f(p,n)
-result *=k
-print(result%1000000007)
+        b = f(x, (y - 1) / 2)
+        return b * b * x % 1000000007
 
 
+k, p, n = list(map(int, sys.stdin.readline().split()))
+# 0.1초당 p배씩 증가이기 때문에 계산을 편리하게 하기위해 n을 10배 해준다.
+n = 10 * n
+result = f(p, n)
+result *= k
+print(result % 1000000007)
