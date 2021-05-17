9b28cbb9b05cdd8f00b134de1d7822ebfee729be
JunJangE <69571848+junjange@users.noreply.github.com>
2021-05-17 18:18:05 +0900

    Update 바이러스.py

--- softeer/바이러스.py
+++ softeer/바이러스.py
@@ -1,9 +1,10 @@
 import sys
 
 k, p, n = list(map(int,sys.stdin.readline().split()))
-result = k
+
 for i in range(n):
-    result = (result * p) % 1000000007
+    k = (k * p) % 1000000007
+
+print(k)
 
 
-print(result)
