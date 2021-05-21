bab17b1d6bd08f157024873133cb9e5c0d4e9e1c
JunJangE <69571848+junjange@users.noreply.github.com>
2021-05-21 20:52:50 +0900

    Update 2437.py

--- baekjoon/2437.py
+++ baekjoon/2437.py
@@ -1,14 +1,18 @@
 import sys
 
 n = int(sys.stdin.readline())
-w = list(map(int,sys.stdin.readline().split()))
+w = list(map(int, sys.stdin.readline().split()))
+# 추의 무게 오름차순 정렬
 w.sort()
+# target의 초기값(양의 정수의 최솟값은 1이기 때문)
 target = 1
-
+# 추의 무게를 더한 target과 다음 추의 무게를 비교
 for i in w:
+    print(target, i)
+    # 추의 무게가 더 크면 그때의 target을 측정할 수 없다.
     if target < i:
         break
-
+    # target의 추의 무게를 더해준다.
     target += i
 
 print(target)
