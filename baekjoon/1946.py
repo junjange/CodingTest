3c226b41a45389813bd251290fff53c23ded68f5
JunJangE <69571848+junjange@users.noreply.github.com>
2021-05-22 12:22:19 +0900

    Update 1946.py

--- baekjoon/1946.py
+++ baekjoon/1946.py
@@ -9,7 +9,7 @@ for _ in range(test_case):
     # 서류심사 성적과 면접시험 성적을 입력받아 서류심사 성적기준으로 정렬한다.
     test = sorted([list(map(int, sys.stdin.readline().strip().split())) for x in range(n)],
                   key=lambda x: x[0])
-    # 서류심사 성적기준으로 오름차순으로 정렬했기 때문에 test[0]번째 지원자는 합격이다.
+    # 서류심사 성적기준, 오름차순으로 정렬했기 때문에 test[0]번째 지원자는 합격이다.
     count = 1
     # 합격한 지원자 면접시험 성적 기준으로 비교한다.
     target = test[0][1]
