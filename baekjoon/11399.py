3ae33a1a2ccc33661b57b944c6fbbc2c413ce554
JunJangE <69571848+junjange@users.noreply.github.com>
2021-05-23 15:51:47 +0900

    Update 11399.py

--- baekjoon/11399.py
+++ baekjoon/11399.py
@@ -1,13 +1,16 @@
 n = int(input())
-
-p =list(map(int, input().split()))
+p = list(map(int, input().split()))
+# 돈을 인출하는데 걸리는 시간을 오름차순으로 정렬한다.
 p.sort()
-
-result =0
-x=0
+# 기다리는 시간
+total = 0
+# 총 걸리는 시간
+result = 0
 
 for i in p:
-    result = i+result
-    x += result
+    # 돈을 인출하는 시간과 기다리는 시간을 더해준다.
+    total = i + total
+    # 기다리는 시간을 총 걸리는 시간에 더해준다.
+    result += total
 
-print(x)
+print(result)
