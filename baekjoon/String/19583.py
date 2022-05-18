c0ab9b03c9af1330cbdb40782280187be99741d4
JunJangE <wnswkd486@naver.com>
2022-05-19 00:19:22 +0900

    Update 19583.py

--- baekjoon/String/19583.py
+++ baekjoon/String/19583.py
@@ -4,17 +4,18 @@ s, e, q = map(str, sys.stdin.readline().split())
 dic = {}
 answer = {}
 
-while True:
-    try:
-        time, nick = map(str, sys.stdin.readline().split())
+# 반복문을 통해 시간과 닉네임을 입력받는다.
+for i in sys.stdin:
+    time, nick = i.rstrip().split()
+    
+    # 개총 시작시간(포함)보다 일찍 들어온 사람을 dic 입력 
+    if time <= s:
+        dic[nick] = time
+        
+    # 개총 종료시간과 스트리밍 종료시간 사이에 들어온 사람을 확인
+    elif e <= time <= q:
+        # 개총 시작시간에보다 일찍 들어온 사람이라면 answer 입력
+        if nick in dic:
+            answer[nick] = 1
 
-        if time <= s:
-            dic[nick] = time
-
-        elif e <= time <= q:
-            if nick in dic:
-                answer[nick] = 1
-
-    except:
-        break
 print(len(answer))
